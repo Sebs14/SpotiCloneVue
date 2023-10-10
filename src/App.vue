2f8f377facef67fb243aae1e07bf03dcde43dce7
Sebs14 <00196919@uca.edu.sv>
2023-10-10 00:22:48 -0600

    feat: :bento: Assets added
    Added a lot of assets as icons and music, and also finished the first stage from the clone as it is the first page of spoticlone

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref } from 'vue'
   import { RouterLink, RouterView } from 'vue-router';
+  import MenuItem from './components/MenuItem.vue';
 
   //icons to be used
   import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
@@ -26,7 +27,7 @@
       justify-between
       "
     >
-      <div class="flex items-center ml-6">
+      <div class="flex items-center ml-6 gap-2">
         <button type="button" class="rounded-full bg-black p-[1px] cursor-pointer">
           <ChevronLeft fillColor="#FFFFFF" :size="30"/>
         </button>
@@ -42,10 +43,64 @@
           <ChevronUp v-else @click="openMenu = false" fillColor="#FFFFFF" :size="25"/>
         </div>
       </button>
-      <span class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer">
-
+      <span v-if="openMenu"
+      class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer">
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]  ">Account</li>
+        </ul>
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]  ">Manage Plan</li>
+        </ul>
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]  ">Profile</li>
+        </ul>
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] ">Private session</li>
+        </ul>
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">Preference</li>
+        </ul>
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">Log out</li>
+        </ul>
       </span>
     </div>
+    <div
+      id="SideNav"
+      class="h-[100%] p-6 w-[240px] fixed z-50 bg-black"
+    >
+      <RouterLink to="/">
+        <img width="125" src="/images/icons/spotify-logo.png"/>
+      </RouterLink>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLink to="/">
+          <MenuItem class="ml-[1px]" :iconSize="23" name="Home" iconString="home" pageUrl="/"/>
+        </RouterLink>
+        <RouterLink to="/search">
+          <MenuItem class="ml-[1px]" :iconSize="24" name="Search" iconString="search" pageUrl="/search"/>
+        </RouterLink>
+        <RouterLink to="/library">
+          <MenuItem class="ml-[2px]" :iconSize="23" name="Library" iconString="library" pageUrl="/library"/>
+        </RouterLink>
+        <div class="py-3.5"/>
+        <MenuItem :iconSize="24" name="Create Playlist" iconString="playlist" pageUrl="/playlist"/>
+        <MenuItem class="-ml-[1px]" :iconSize="27" name="Liked Songs" iconString="liked" pageUrl="/liked"/>
+      </ul>
+      <div class="border-b border-b-gray-700"/>
+      <ul>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Playlist #1</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Playlist #2</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Playlist #3</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">Playlist #4</li>
+      </ul>
+    </div>
+  </div>
+  
+  <div
+    class="fixed right-0 top-0 w-[calc(100%-240px)] h-full bg-gradient-to-b from-[#1C1C1C] to-black "
+  >
+
   </div>
 </template>
 
